412c8202fbee6c496396d76792bb595cedee60b6
Emmanpogi04 <121656643+IEMDomain04@users.noreply.github.com>
2024-01-15 21:11:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+        
+        window.addEventListener("scroll", function () {
+        var nav = document.querySelector("nav");
+        nav.classList.toggle("sticky", window.scrollY > 0);
+        })
+
+        function MinecraftCharacters() {
+      window.location.href = "MC_Characters.html";
+        }
+
+        function MinecraftHistory() {
+            window.location.href = "MC_History.html";
+        }
+
+        function MinecraftBlocks() {
+            window.location.href = "MC_Blocks.html";
+        }
